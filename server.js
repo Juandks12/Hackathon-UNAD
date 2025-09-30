7e95277c9fb9f054da2913317600bae370df33b8
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-09-30 09:58:12 -0500

    Implementación de autenticación, gestión de usuarios y registros de asistencia

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const authRoutes = require("./routes/auth");
-const employeeRoutes = require("./routes/employee");
-const adminRoutes = require("./routes/admin");
+const authRoutes = require("./src/routes/auth");
+const employeeRoutes = require("./src/routes/employee");
+const adminRoutes = require("./src/routes/admin");
 
-const { verifyToken } = require("./middleware/auth");
-const { checkRole } = require("./middleware/roles");
+const { verifyToken } = require("./src/middleware/auth");
+const { checkRole } = require("./src/middleware/roles");
 
 const app = express();
 app.use(bodyParser.json());
